d8e3a23edc1efe888e6fbe807d79e4ebcfdc6e61
junruizhang <junrui@junruizhangdeMacBook-Pro.local>
2023-10-18 20:06:46 +0800

    feat: first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="box">
+    <div>2123</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, reactive } from 'vue';
+</script>
+
+<style lang="less" scoped>
+.box {
+}
+</style>
