a69cc439a055613aa9ab2e87f92bc75220ca0fe4
junruizhang <junrui@junruizhangdeMacBook-Pro.local>
2023-10-18 21:57:11 +0800

    feat: 完成初始环境搭建

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="box">
-    <div>2123</div>
+    <van-button type="primary">2123</van-button>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, reactive } from 'vue';
+import Web3 from 'web3';
+var web3 = new Web3(Web3.givenProvider || 'ws://localhost:8545');
+
+console.log(web3);
 </script>
 
 <style lang="less" scoped>
--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,7 @@ export default defineConfig({
       resolvers: [VantResolver()],
     }),
   ],
+  resolve: {
+    extensions: ['.js', 'jsx', '.ts', '.ejs', '.mjs', '.cjs'],
+  },
 });
